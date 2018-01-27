eccf9acc2fab72e3a5927365710997f027983236
Johnathan Fong <johnathan.fong@climaxmedia.com>
2018-01-27 17:09:41 -0500

    import google fonts

--- header.php
+++ header.php
@@ -16,6 +16,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+  <link href="https://fonts.googleapis.com/css?family=Lora|Open+Sans" rel="stylesheet">
+
 
 	<?php wp_head(); ?>
 </head>
